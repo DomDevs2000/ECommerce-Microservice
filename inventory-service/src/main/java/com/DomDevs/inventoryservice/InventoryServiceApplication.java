fde964e9fb6dfa3ba749e222e833026296331a07
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-23 18:48:37 +0100

    created bean that save inventory data to db

--- inventory-service/src/main/java/com/DomDevs/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/DomDevs/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,36 @@
 package com.DomDevs.inventoryservice;
 
+import com.DomDevs.inventoryservice.model.Inventory;
+import com.DomDevs.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
 
-}
+
+    }
+
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("iphone_12");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("iphone_13_red");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+
+        };
+
+    }
+};
