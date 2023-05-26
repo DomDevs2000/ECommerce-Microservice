b7f5c8548416885c8e2a415b63dc14f4ca10ce23
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-26 14:43:40 +0100

    implemented circuit breaker to Order Controller | added fallback method

--- order-service/src/main/java/com/DomDevs/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/DomDevs/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.DomDevs.orderservice.controller;
 
 import com.DomDevs.orderservice.dto.OrderRequest;
 import com.DomDevs.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,17 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something Went Wrong, Please Try Again Later";
+
+    }
+
+
 }
 
