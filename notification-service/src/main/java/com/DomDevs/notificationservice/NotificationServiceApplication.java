724fee966ac430fad94b419acf0b0f0eb25b0ec2
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-26 19:54:29 +0100

    added logger to handleNotification method

--- notification-service/src/main/java/com/DomDevs/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/DomDevs/notificationservice/NotificationServiceApplication.java
@@ -1,10 +1,14 @@
 package com.DomDevs.notificationservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
@@ -12,8 +16,8 @@ public class NotificationServiceApplication {
     }
 
     @KafkaListener(topics = "notificationTopic")
-    public void handleNotification(OrderPlacedEvent orderPlacedEvent){
-
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Received Notification for Order - {} ", orderPlacedEvent.getOrderNumber());
     }
 
 }
