d1215503335c53b89351404243ce12fcafca45b8
DomDevs2000 <aidan@carvalho.org.uk>
2023-06-01 16:40:27 +0100

    added lombok builder annotation

--- order-service/src/main/java/com/DomDevs/orderservice/dto/OrderLineItemsDto.java
+++ order-service/src/main/java/com/DomDevs/orderservice/dto/OrderLineItemsDto.java
@@ -1,8 +1,7 @@
 package com.DomDevs.orderservice.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +10,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderLineItemsDto {
     private Long id;
     private String skuCode;
