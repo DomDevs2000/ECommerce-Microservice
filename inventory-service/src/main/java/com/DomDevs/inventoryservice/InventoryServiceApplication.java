861633b5a68338b5ad5b2bcdddba8e388e1068e0
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-07-05 17:03:37 +0100

    commented out Beans that add items to inventory at application startup

--- inventory-service/src/main/java/com/DomDevs/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/DomDevs/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.DomDevs.inventoryservice;
 
+import com.DomDevs.inventoryservice.model.Inventory;
+import com.DomDevs.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,20 +14,20 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 //    creates bean that saves inventory to db on app startup
-//    @Bean
-//    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-//        return args -> {
-//            Inventory inventory = new Inventory();
-//            inventory.setSkuCode("iphone_12");
-//            inventory.setQuantity(100);
-//
-//            Inventory inventory1 = new Inventory();
-//            inventory1.setSkuCode("iphone_12_red");
-//            inventory1.setQuantity(0);
-//
-//            inventoryRepository.save(inventory);
-//            inventoryRepository.save(inventory1);
-//
-//        };
-//    }
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("iphone_12");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("iphone_12_red");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+
+        };
+    }
 };
